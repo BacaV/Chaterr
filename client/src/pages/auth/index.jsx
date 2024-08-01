261cab52bbe624cd99dacd98897936a5da59bb4a
BacaV <nikolajevtic3405@gmail.com>
2024-08-02 01:34:09 +0200

    initial commit

---
+++ client/src/pages/auth/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Auth = () => {
+  return (
+<>
+    <div className='flex w-screen h-screen items-center justify-center bg-cyan-200'>
+      <div className=' bg-lime-50 w-1/2 h-1/2 rounded-lg '></div>
+    </div>
+</>
+
+  )
+}
+
+export default Auth
---
+++ client/src/pages/profile/index.jsx
@@ -0,0 +1,10 @@
+
+export const Profile = () => {
+  return (
+    <div>Profile
+    
+    </div>
+  )
+}
+
+export default Profile
