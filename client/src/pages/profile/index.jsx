fd2d8f37ffadc710ded43fb0a2327f403c660e09
BacaV <nikolajevtic3405@gmail.com>
2024-08-11 23:21:39 +0100

    Profile styling middle

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,6 +1,10 @@
 import { useAppStore } from "@/store";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 
 
 export const Profile = () => {
@@ -11,6 +15,7 @@ export const Profile = () => {
   const [image, setImage] = useState('');
   const [color, setColor] = useState('');
   const [hovered, setHovered] = useState('');
+  const [email, setEmail] = useState('');
 
   const saveChanges = async () => {
 
@@ -21,13 +26,35 @@ export const Profile = () => {
     <>
       
       <div className="h-[100vh] w-[100vw] bg-[#fefefe] flex items-center justify-center">
-        <div className="h-[300px] w-[300px] gap-10 bg-gradient-to-br drop-shadow-2xl from-[#a7c957] to-[#dffa9f]/60 text-white flex items-center justify-center">
-          <div className="avatar w-[64px] h-[64px] border-2">
-            <img src={image} alt="Avatar" />
+        <div className='bg-gradient-to-br drop-shadow-2xl from-[#a7c957] to-[#dffa9f]/60 w-[80vw] md:w-[90vw] lg:w-[70vw] h-[80vh] rounded-xl flex flex-col justify-center items-center gap-10'>
+        <h2 className="text-3xl font-bold text-white">Profile Settings</h2>
+        <div className="flex justify-center items-center gap-10 ">
+          <div className="flex flex-col gap-3">
+            <Avatar className="size-40">
+              <AvatarImage src={image} />
+              <AvatarFallback>Us</AvatarFallback>
+            </Avatar>
+
+            <Button variant="outline" onClick={() => setHovered('')}>
+              Change Avatar 
+            </Button>
           </div>
-          <div className="settings">
-            <h3 className="text-xl font-bold">Place Holder</h3>
+         
+
+          <div className="settings flex flex-col gap-3">
+            <Input className="w-full" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <Input className="w-full" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+            <Input className="w-full" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+            <div className="flex gap-3">
+              <button className="red w-[35px] h-[35px] bg-red-500 rounded-full"></button>
+              <button className="green w-[35px] h-[35px] bg-green-500 rounded-full"></button>
+              <button className="blue w-[35px] h-[35px] bg-blue-500 rounded-full"></button>
+              <button className="yellow w-[35px] h-[35px] bg-yellow-500 rounded-full"></button>
+              <button className="purple w-[35px] h-[35px] bg-purple-500 rounded-full"></button>
+            </div>
+            <Button className='w-full bg-purple-700' onClick={saveChanges}>Save Changes</Button>
           </div>
+        </div>
         </div>
       </div>
 
